510aca451c7614911f97043c153d8553fdee8556
Virpi <virpi.sumu@gmail.com>
2016-12-27 23:58:29 +0200

    testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,7 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Account-taulun testidata
+INSERT INTO Account (firstname, password) VALUES ('Ada', '123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Account (firstname, password) VALUES ('Charles', '123');
+-- Item-taulun testidata
+INSERT INTO Item (title, itemtype, added) VALUES ('Logicomix', 'kirja', NOW());
+-- Loan-taulun testidata
+INSERT INTO Loan (checkedout, account_id, item_id) VALUES (NOW(), 1, 1);
