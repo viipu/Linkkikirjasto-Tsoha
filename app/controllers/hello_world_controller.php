ac843ddcd3a24f1695f58cf4c84d7377bb762616
Virpi <virpi.sumu@gmail.com>
2016-12-27 23:21:12 +0200

    dokumentaatio päivitetty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,34 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+//   	  View::make('home.html');
+        View::make('suunnitelmat/index.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+//      echo 'Hello World! Hiekkalaatikko!';
+        View::make('helloworld.html');
     }
-  }
+
+    public static function items_list() {
+        View::make('suunnitelmat/items_list.html');
+    }
+
+    public static function item_show() {
+        View::make('suunnitelmat/item_show.html');
+    }
+
+    public static function loans_list() {
+        View::make('suunnitelmat/loans_list.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+//}
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+$routes->get('/loans', function() {
+    HelloWorldController::loans_list();
+});
+$routes->get('/loans/1', function() {
+    HelloWorldController::item_show();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+$routes->get('/items', function() {
+  HelloWorldController::items_list();
+});
+$routes->get('/items/1', function() {
+  HelloWorldController::item_show();
+});
